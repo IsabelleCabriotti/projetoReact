3159838df14226f10c59bc3257b743c0eaab2857
IsabelleCabriotti <isahcabriotti@gmail.com>
2025-06-02 21:12:33 -0300

    Projeto Loja

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,7 @@ const Home = () =>{
                         <li><Link to='Atv03'>Semana 3</Link></li>
                         <li><Link to='Atv04'>Semana 4</Link></li>
                         <li><Link to='Atv05'>Semana 5</Link></li>
+                        <li><Link to='loja'>Loja</Link></li>
                     </ul>
                 </nav>
             </div>
